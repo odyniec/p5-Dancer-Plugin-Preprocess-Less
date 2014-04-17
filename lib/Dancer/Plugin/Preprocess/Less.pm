f36a92999d53eb606f32699e63bbd897bb462aed
Michal Wojciechowski <odyniec@cpan.org>
2014-04-17 23:55:01 +0200

    Add abstract and synopsis

--- lib/Dancer/Plugin/Preprocess/Less.pm
+++ lib/Dancer/Plugin/Preprocess/Less.pm
@@ -3,9 +3,44 @@ package Dancer::Plugin::Preprocess::Less;
 use strict;
 use warnings;
 
-# FIXME: Add ABSTRACT
+# ABSTRACT: Generate CSS files from .less files
 
 # VERSION
 
+use CSS::LESSp;
+
 1;
 
+__END__
+
+=head1 SYNOPSIS
+
+Dancer::Plugin::Preprocess::LESS automatically generates CSS files from Less
+files in a Dancer web application.
+
+Add the plugin to your application:
+
+    use Dancer::Plugin::Preprocess::Less;
+
+Configure its settings in the YAML configuration file:
+
+    plugins:
+      "Preprocess::Less":
+        save: 1
+        paths:
+          - css
+          - subdir/css
+
+=head1 SEE ALSO
+
+=for :list
+
+* L<http://lesscss.org/> - Less website
+
+* L<CSS::LESSp>
+
+=head1 ACKNOWLEDGEMENTS
+
+The plugin uses Ivan Drinchev's L<CSS::LESSp> module. 
+
+=cut
